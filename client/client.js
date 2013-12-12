a9637e062e17a92870c5d56c8251a4a5d02803c0
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2013-12-12 11:08:04 +0000

    pitch selection done

--- client/client.js
+++ client/client.js
@@ -11,44 +11,64 @@ myDep = function(initial) {
 };
 
 myDep.prototype = {
-    dep: new Deps.Dependency(),
-    get: function () {
-        this.dep.depend();
-        return this.value;
-    },
-    set: function (newValue){
-        if (this.value !== newValue) {
-            this.value = newValue;
-            this.dep.changed();
-        }
-        return this.value;
+  dep: new Deps.Dependency(),
+  get: function () {
+    this.dep.depend();
+    return this.value;
+  },
+  set: function (newValue){
+    if (this.value !== newValue) {
+      this.value = newValue;
+      this.dep.changed();
     }
+    return this.value;
+  }
 };
 
 mapCenter = new myDep([51.5080391, -0.12806929999999284]);
-circleSize = new myDep(800);
+circleSize = new myDep(8000);
 venues = new myDep([]);
+
+mapCenter.set = function(newValue){
+  if (this.value !== newValue) {
+    this.value = newValue;
+    this.dep.changed();
+//    console.log(this, printStackTrace())
+  }
+  return this.value;  
+}
 
 initialize = function() {
 
-  myLocation = new google.maps.LatLng(51.5080391, -0.12806929999999284);
+  var defaultLocation = new google.maps.LatLng(51, 0);
   var mapOptions = {
     zoom: 11,
-    center: myLocation
+    center: defaultLocation
   };
   pitchMap = new google.maps.Map(document.getElementById('pitchMap'),
       mapOptions);
   gc = new google.maps.Geocoder();
-  navigator.geolocation.getCurrentPosition(function(res) {
-    myLocation = new google.maps.LatLng(res.coords.latitude, res.coords.longitude);
-    pitchMap.setCenter(myLocation);
-    Meteor.call('pitchesWithin', {"lat": res.coords.latitude, "lng": res.coords.longitude}, 8000, function(err, res) {
-      if (err) console.log(err);
-      else venues.set(res);
+  if (Meteor.user().profile.player.center) {
+    pitchMap.setCenter(new google.maps.LatLng(Meteor.user().profile.player.center.nb, Meteor.user().profile.player.center.ob));  
+    mapCenter.set(pitchMap.getCenter());
+    circleSize.set(Meteor.user().profile.player.size);
+    $('#distanceWrite').val(circleSize.get()/100);
+    $('#distanceRead').html(circleSize.get()/1000+'km'); 
+  }
+  else {
+    navigator.geolocation.getCurrentPosition(function(res) {
+      var myLocation = new google.maps.LatLng(res.coords.latitude, res.coords.longitude);
+      mapCenter.set(myLocation);
+      if (liveCircle) liveCircle.setCenter(myLocation);
+      pitchMap.setCenter(myLocation);
+      Meteor.call('pitchesWithin', {"lat": res.coords.latitude, "lng": res.coords.longitude}, 8000, function(err, res) {
+        if (err) console.log(err);
+        else venues.set(res);
+      });
+    }, function() {
+      window.alert("Your browser does not support geolocation, so you'll have to use the address bar to find your location.")
     });
-  }, function() {
-    window.alert("Your browser does not support geolocation, so you'll have to use the address bar to find your location.")
-  });
+  }
   var pitches = Pitches.find().fetch();
   for (var i=0; i < pitches.length; i++) {
     var marker = new google.maps.Marker({
@@ -59,7 +79,6 @@ initialize = function() {
   }
   mapCenter.set(pitchMap.getCenter());
   google.maps.event.addListener(pitchMap, 'center_changed', function() {
-//    mapCenter.set(pitchMap.getCenter());
   });
 }
 
@@ -68,6 +87,10 @@ function loadScript() {
   script.type = 'text/javascript';
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
       'callback=initialize';
+  document.body.appendChild(script);
+  script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "https://raw.github.com/stacktracejs/stacktrace.js/master/stacktrace.js";
   document.body.appendChild(script);
 }
 
@@ -113,12 +136,14 @@ Template.defineBounds.events({
         mapCenter.set(liveCircle.getCenter());
       });
     }
+  },
+  'click #cancelOrSave .green.button': function() {
+    Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.player': {center: mapCenter.get(), size: circleSize.get(), venues: venues.get().map(function(v) {return v._id;})}}});
   }
 });
 Template.defineBounds.rendered= function() {
-  $('#distanceWrite').val(80);
-  $('#distanceRead').html(parseInt($('#distanceWrite').val(), 10) / 10 + 'km');
-  circleSize.set(parseInt($('#distanceWrite').val(), 10) * 100);
+  $('#distanceWrite').val(circleSize.get()/100);
+  $('#distanceRead').html(parseInt($('#distanceWrite').val(), 10)/10 + 'km');
 };
 
 
@@ -154,20 +179,3 @@ Deps.autorun(function() {
     });
   }
 });
-
-function updateCircle() {
-  var populationOptions = {
-    strokeColor: '#0000FF',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#0000FF',
-    fillOpacity: 0.35,
-    map: pitchMap,
-    center: pitchMap.getCenter(),
-    radius: parseInt($('#distanceWrite').val(), 10)*100
-  };
-  if (liveCircle) {
-    liveCircle.setMap(null);
-  }
-  liveCircle = new google.maps.Circle(populationOptions);
-}
