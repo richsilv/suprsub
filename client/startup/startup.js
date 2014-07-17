616ab75f9f5676595714d1ddab8c4eacb326e4b8
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-07-17 10:38:08 +0100

    corrected pitch list update problem

--- client/startup/startup.js
+++ client/startup/startup.js
@@ -3,7 +3,7 @@ Meteor.startup(function() {
 //	clientFunctions.logTemplateEvents();
 
 	Deps.autorun(function() {
-		if (appVars.mapCenter.get() && typeof(appVars.circleSize) !== 'undefined' && appVars.circleSize !== null && appVars.circleSize.get() && Subs.pitches && Subs.pitches.ready()) {
+		if (appVars.mapCenter.get() && typeof(appVars.circleSize) !== 'undefined' && appVars.circleSize !== null && appVars.circleSize.get() && appVars.pitchesReady.value) {
 			console.log("updating pitch list");
 			Meteor.call('pitchesWithin', {"lat": appVars.mapCenter.get().lat(), "lng": appVars.mapCenter.get().lng()}, appVars.circleSize.get(), function(err, res) {
 				if (err) console.log(err);
