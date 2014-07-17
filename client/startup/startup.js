d37c58fda7977e319dcd2204a2fb0cfd550f20cb
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2014-07-17 10:34:36 +0100

    changed pitchesWithin calls to client side

--- client/startup/startup.js
+++ client/startup/startup.js
@@ -3,9 +3,10 @@ Meteor.startup(function() {
 //	clientFunctions.logTemplateEvents();
 
 	Deps.autorun(function() {
-		if (appVars.mapCenter.get() && typeof(appVars.circleSize) !== 'undefined' && appVars.circleSize !== null && appVars.circleSize.get() && Subs.pitches && Subs.pitches.ready()) {
+		console.log("autorun initialised");
+		if (appVars.mapCenter.get() && typeof(appVars.circleSize) !== 'undefined' && appVars.circleSize !== null && appVars.circleSize.get() && appVars.pitchesReady.value) {
 			console.log("updating pitch list");
-			Meteor.call('pitchesWithin', {"lat": appVars.mapCenter.get().lat(), "lng": appVars.mapCenter.get().lng()}, appVars.circleSize.get(), function(err, res) {
+			clientFunctions.pitchesWithin({"lat": appVars.mapCenter.get().lat(), "lng": appVars.mapCenter.get().lng()}, appVars.circleSize.get(), function(err, res) {
 				if (err) console.log(err);
 				else if ('venues' in appVars) {
 					appVars.venues.set(res);
